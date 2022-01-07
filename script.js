b584d1d901fb2ae8cd8f562384c6540a07b8efd0
anton76krikke <73478659+anton76krikke@users.noreply.github.com>
2022-01-07 22:24:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const age = (45);
+if (age => 18) {
+    console.log("je mag lekker drinken")
+
+}
+else { console.log("nog een jaartje wachten") }
+
+const IsFemale = true;
+if (IsFemale) {
+    console.log("je bent een vrouw, welkom")
+}
+else { console.log("mannen"); }
+
+const driverStatus = ('designted');
+if (driverStatus == ('designated')) {
+    console.log("je bent chauffeur")
+}
+else { console.log("brokkenpiloot niet geschikt "); }
